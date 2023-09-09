2322ef3bcdd0b5314abd08e92191fcd60894dc98
sweetARTdev <anuoluwapotenibiaje@gmail.com>
2023-09-09 23:42:12 +0100

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const currentDayElement = document.querySelector(".current_day");
+
 const currentUTCTimeElement = document.querySelector(".utc-time");
 
-// JavaScript code to get the current day of the week
 const daysOfWeek = [
   "Sunday",
   "Monday",
@@ -12,12 +12,11 @@ const daysOfWeek = [
   "Saturday",
 ];
 const currentDate = new Date();
+
 const currentDay = daysOfWeek[currentDate.getDay()];
 
-// Update the element with the current day
 currentDayElement.textContent = currentDay;
 
 const currentUTCTimeMillis = Date.now();
 
-// Update the element with the current UTC time in milliseconds
 currentUTCTimeElement.textContent = currentUTCTimeMillis;
